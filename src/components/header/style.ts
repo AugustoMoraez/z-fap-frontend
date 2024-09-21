a9dbce1ff9d757d9c33d4756babf0f2c94bd2519
Augusto Moraes <supergamerbros2015@gmail.com>
2024-09-21 11:10:02 -0300

    update repo

--- src/components/header/style.ts
+++ src/components/header/style.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     width: 100%;
+    max-width: 1040px;
     height: 120px;
     background-color: royalblue;
     display: flex;
